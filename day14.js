23307c73d9f17c649b26f6e2361d5b70a65d1cf5
Mark Huggins <mhuggins@glgroup.com>
2021-12-15 00:48:53 -0500

    day14 part 2
    inspiration from to get past the performance issues
    https://tranzystorek-io.github.io/paste/#XQAAAQA/EwAAAAAAAAA9iImGVD/UQZfk+oJTfwg2/VsJ0DpNkr2zGUvTQlsh3wS86ErCiIs+8hruVNIFt25kOOZrzOGZxbRwHmJ5wrAVdOuA2kk0mNS0CgC1DvMoV95fu0jZGBVOkQsA/4KOsC4R1JysGd2uHnbHhqMmLEKL5Ji2P+sXBMFsoPaHc8dt2za7mYlecB5/ryLs00mU6Ln6nYJP/WdwOKom1qZit0yUeeM4UyZwB7Tae4pHIciZOFN0eQqq4QiydBFQlwXIkRGhnQITube/ulnAKSwTqiqFqM3NNwXs6Gbaqfo2VL1+uTuYKBXO+dTKYlutqjH7kYHbBTQog0PSwS2v8G7Xk1Ovsgg7+YH4Qn60FqgjhCdY3yuq9NdNuNHcljLAUMlwpyelM83nestvv26yxP0lhiYBALUUfHxDHpokedTSQ1FJLf1Ezq1cxDhZnZ5gJAErAV5NZIVC2imk053s7A34IyNx8lEhlZRdmUiHjLIwEHpGMl9N2jHGVZuYVi+VOxuuXt5cQ1Wm5N2v26RA2uzR2fdt3wViQymhRcGieqs848kUH43jNN4u+kGGsLariGrM+CDxarj4Qv3ZNSBw58S4atitdtpsm/vrYQPSZUH4Fvc6OTnykKEam8FhUm4/GvwYdAEKHV17f9sFO68Fro9uSwaOeD5DzB8/s780ORBSE4KntvLHXtlMT7EExNoEkLkt5elOh1YDtSHbe/nzAfGjftjLkzeEriFYZHCu5AknXiowwit+dhVHCIhcBtqcwcjMFaSW4JWXkipOgOkqNSE4fqewInzbzSnRIaGOTqyMTKVRldp4Mn4Jhhdfon0GVbjn7zFiofV9adOQJhuavlEtlaYOXblpUrfX+1p6OzozBK++rd/cgCROo2RSwqJmpd7PdeRfeuMceZ02qPe/D58xD4AxTKcGu8ynzS+FdwnHLxDnN1DoNp0pyivUgIAW0xGxwr/Lxwr2HEQaSCeHzDeoosyLnvIOpeJiYZtYBvzo7fSscvkk+shdKE4R+HWVqagsmTLisdXsnl2Nfr48B5qqZdGd1XmICcxz4xzVNIh5ZtycR4iff4AR+ffaeV12o0QJJnv8hj4Fx1Lx0saKuEmLbE/4urtRoKp/bybT/F4nsZDjYxS6CPGO/drFrrueTxroKgQI/lqhJ53BOu1EVxlz2iOSOeCh7NlxAvQy5E65siKadI9jH/8SUCzaWycaMU4vRVEuWvHn926yENDe4Fdn3pdh5dXmidnTdfO4hgMl8/DspqD8P81QY7nLVxkl/1qcgAfJhbnJiJBvvy4C28bEaCPyVdlXeQpsf/IvsRMYakQO0s5MdvGBXwBQrD09soKW1Q7yFvu2sF/goZxu2rdNChVBvGb1FoLDwLkByQP204Tbo3Qsi43lHR1p5RZOE7wru1/ozGMpcgoYSSfWyE1i3LoSsDY9sH8Lce2XM7Im4xzWZRy3vaswdbgk/XVvJV7Sj2FcXKKEnVs/vkcyJDWZKtVj8abeMFG4GmLc4/TSmrr+pEeOc3v7XlFOijluNCkis/1YEpNqFTE35l4o/MV8BIiAqoYUgcvzZspoW4ZHMncr75bQWrPeHqiL1zpnxr+NfvUMLCbEgDW0OGpNpUH2NrkOEEee/P36BgGVFVUt5yFHoNGPyX2z3F57cuL8A1E3ozgqI5wT4uSbbxfJUf99CjiE7xPrf1N+ATQYU6u/AornJR6t/0nxZbrH3Oet5D0VLqXGqmrbviWHYwqm5b9isIJ256MvYxiKRh05B78Mro/ujgv6XZc4q13lJKZ+F0mtQXFVUAxI7CPvebhoAW7uUtMSG9thyI9rYaLgaoypMfUtRUsRRxfVq9k1UtYXvQsQubgTIHVFaBmjmOty6ztwjugLWyoBJX7YFIbjQnUWHJgldy8rihvAJfplZKsvbbm3y1eahMXpdzaf4KO0j4C5TfkOdHsfWAr2qTnUpl22rUiJqBSIp+b2WdEEgHmLY5+zrr2IZIaIGmbxZy/Xwhas0bICFsbY0x1cEnSqEkSs4euqnxBHr5sekQnu5x5/V65MEiGhzvGZJWiju7jewi812TXazaovDzUbAAmmHraQRF1ZIQKVFUDFuKJJoaLSmxic8K2TrMr808ugcuEKD+8YaU1rPk7InLGtGylYcu6jFwXuA6mkZg7C+vn51z4GS5TGidK3nfvpBtYk6i82Hg7+a6C28dfUxanvFm9jLEjTW6614U09bxjZYLnRql23SpPD/2UXM0VPCZVixQtvJozW/wxxzBQ0yhSl5UbFL+JtyuiZGFBvpllv+NRs7jnID6m36yi68Ywj4R39YOA1pNmBI4cGAlDSExuakbGYL4qpdPo48a84Z202CnQv0ZfRklPvemPMVOqHIIxmC5uHbk6sVoynqFhLvcsJSuBdvgt2NM/eayfYD5vxAsAVN+rAt6cAv9OcRUQW7P/yI2tF

--- day14.js
+++ day14.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 
-const rawData = fs.readFileSync("./data/day14.test", "utf8").split("\n").filter(Boolean)
+const rawData = fs.readFileSync("./data/day14.txt", "utf8").split("\n").filter(Boolean)
 
 const polymerTemplate = rawData.splice(0, 1)[0]
 
@@ -12,51 +12,47 @@ rawData.forEach((mapItem) => {
 })
 
 function findPolymer(iterations) {
-  const
+  let pairCounts = new Map()
   let template = polymerTemplate
-  for (let i = 0; i < iterations; i++) {
-    console.log(template.length)
-    console.log(i)
-    let matches = []
-    insertRules.forEach((value, key) => {
-      const regex = new RegExp(`(?=(${key}))`, "g")
-      matches = [...matches, [...template.matchAll(regex)]]
-    })
-    console.log(`gotmatches`)
-    matches = matches.flat().sort((a, b) => a.index - b.index)
-    templateArray = template.split()
-    console.log(`arraySplit`)
-    matches
-      // .sort((a, b) => a.index - b.index)
-      .forEach((match, indexCount) => {
-        templateArray.splice(match.index + indexCount, 0, insertRules.get(match[1]))
-        // template = `${template.substring(0, match.index + indexCount + 1)}${insertRules.get(
-        //   match[1]
-        // )}${template.substring(match.index + indexCount + 1)}`
-      })
-    console.log(`updated string array`)
-    template = templateArray.join("")
-    console.log("template joined back to string")
-  }
+  //get initial pair values
+  let matches = []
+  insertRules.forEach((value, key) => {
+    console.log(key)
+    const regex = new RegExp(`(?=(${key}))`, "g")
+    matches = [...matches, [...template.matchAll(regex)]]
+  })
+  matches.flat().forEach((match) => {
+    pairCounts.set(match[1], (pairCounts.get(match[1]) || 0) + 1)
+  })
 
-  return [
-    ...template
-      .split("")
-      .reduce((acc, letter) => {
-        const currentCount = acc.get(letter)
-        if (currentCount) {
-          acc.set(letter, currentCount + 1)
-        } else {
-          acc.set(letter, 1)
-        }
-        return acc
-      }, new Map())
-      .values(),
-  ].sort((a, b) => b - a)
+  for (let i = 0; i < iterations; i++) {
+    let newPairs = new Map()
+    pairCounts.forEach((count, pair) => {
+      charToInsert = insertRules.get(pair)
+      const [firstChar, secondChar] = pair.split("")
+      const firstNewPair = `${firstChar}${charToInsert}`
+      const secondNewPair = `${charToInsert}${secondChar}`
+      newPairs.set(firstNewPair, (newPairs.get(firstNewPair) || 0) + count)
+      newPairs.set(secondNewPair, (newPairs.get(secondNewPair) || 0) + count)
+    })
+    pairCounts = newPairs
+  }
+  const charCounts = [...pairCounts.entries()].reduce((acc, entry) => {
+    const [pair, c] = [...entry]
+    const [a, b] = [...pair.split("")]
+    acc.set(a, (acc.get(a) || 0) + c)
+    acc.set(b, (acc.get(b) || 0) + c)
+
+    console.log(acc)
+    return acc
+  }, new Map())
+
+  return [...charCounts.values()].sort((a, b) => b - a)
 }
 
-// part1Counts = findPolymer(10)
+part1Counts = findPolymer(10)
+console.log(`Part1: ${Math.ceil(part1Counts.at(0) / 2) - Math.ceil(part1Counts.at(-1) / 2)}`)
 // console.log(`Part1: ${part1Counts.at(0) - part1Counts.at(-1)}`)
 
 part2Counts = findPolymer(40)
-console.log(`Part2: ${part2Counts.at(0) - part2Counts.at(-1)}`)
+console.log(`Part2: ${Math.ceil(part2Counts.at(0) / 2) - Math.ceil(part2Counts.at(-1) / 2)}`)
